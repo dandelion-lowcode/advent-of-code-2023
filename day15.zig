e3d73be0dd5c6304451e74165ab52d5f393451c5
Dandelion Lowcode <fmartinez@uground.com>
2023-12-17 17:17:37 +0100

    Day15 - naming conventions

--- day15.zig
+++ day15.zig
@@ -20,9 +20,9 @@ pub fn main() !void {
 }
 
 pub fn part1(input: []const u8) u64 {
-    var commaSplit = std.mem.tokenizeScalar(u8, input, ',');
+    var comma_iterator = std.mem.tokenizeScalar(u8, input, ',');
     var res: u64 = 0;
-    while (commaSplit.next()) |line| {
+    while (comma_iterator.next()) |line| {
         res += getHash(line);
     }
     return res;
@@ -40,27 +40,27 @@ pub fn part2(input: []const u8) u64 {
         defer box.deinit();
     }
 
-    var commaSplit = std.mem.tokenizeScalar(u8, input, ',');
-    while (commaSplit.next()) |instructionStr| {
+    var comma_iterator = std.mem.tokenizeScalar(u8, input, ',');
+    while (comma_iterator.next()) |instructionStr| {
         const instruction = getInstruction(instructionStr);
         const tag = instruction.tag;
 
         const hash = getHash(tag);
         const box = &boxes[hash];
-        const hasSlotIdx = slotWithMatchingKeyIndex(box, tag);
+        const matching_slot = slotWithMatchingKeyIndex(box, tag);
 
         switch (instruction.command) {
             Command.decrease => {
-                if (hasSlotIdx) |idx| {
+                if (matching_slot) |idx| {
                     _ = box.orderedRemove(idx);
                 }
             },
             Command.focalLength => |focalLength| {
-                if (hasSlotIdx) |idx| {
+                if (matching_slot) |idx| {
                     box.items[idx].value = focalLength;
                 } else {
-                    const newSlot: Slot = .{ .tag = tag, .value = focalLength };
-                    boxes[hash].append(newSlot) catch unreachable;
+                    const new_slot: Slot = .{ .tag = tag, .value = focalLength };
+                    boxes[hash].append(new_slot) catch unreachable;
                 }
             },
         }
@@ -70,8 +70,8 @@ pub fn part2(input: []const u8) u64 {
 
 pub fn slotWithMatchingKeyIndex(box: *Box, tag: []const u8) ?usize {
     for (box.items, 0..) |item, idx| {
-        const otherTag = item.tag;
-        if (std.mem.eql(u8, tag, otherTag)) {
+        const other_tag = item.tag;
+        if (std.mem.eql(u8, tag, other_tag)) {
             return idx;
         }
     }
@@ -81,25 +81,25 @@ pub fn slotWithMatchingKeyIndex(box: *Box, tag: []const u8) ?usize {
 pub fn getScore(boxes: *Boxes) u64 {
     var res: u64 = 0;
     for (boxes, 0..) |box, boxIdx| {
-        for (box.items, 0..) |item, itemIdx| {
-            res += (boxIdx + 1) * (itemIdx + 1) * item.value;
+        for (box.items, 0..) |slot, slotIdx| {
+            res += (boxIdx + 1) * (slotIdx + 1) * slot.value;
         }
     }
     return res;
 }
 
 pub fn getInstruction(line: []const u8) Instruction {
-    const hasEqual = std.mem.indexOfPosLinear(u8, line, 0, "=");
-    if (hasEqual) |_| {
+    const contains_equal_sign = std.mem.indexOfPosLinear(u8, line, 0, "=");
+    if (contains_equal_sign) |_| {
         return getFocalLengthInstruction(line);
     }
     return getDecreaseInstruction(line);
 }
 
 pub fn getFocalLengthInstruction(line: []const u8) Instruction {
-    var equalSplitter = std.mem.split(u8, line, "=");
-    const key = equalSplitter.next() orelse unreachable;
-    const valueString = equalSplitter.next() orelse unreachable;
+    var equal_iterator = std.mem.split(u8, line, "=");
+    const key = equal_iterator.next() orelse unreachable;
+    const valueString = equal_iterator.next() orelse unreachable;
     const value = std.fmt.parseInt(u8, valueString, 10) catch unreachable;
 
     return Instruction{ .tag = key, .command = .{ .focalLength = value } };
